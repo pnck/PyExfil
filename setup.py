6f7ef6de9ffdc56bb441cc5c01605a7c5a0c5342
pnck <hio131@gmai.com>
2019-07-12 23:21:03 +0800

    tmp

--- setup.py
+++ setup.py
@@ -12,10 +12,9 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
-required = ['requests>=1.0.0', 'impacket>=0.9.0', 'slackclient', 'progressbar', 'zlib', 'numpy', 'PIL', 'pytube', 'hashlib',
-            'urllib2', 'PyCrypto', 'ftplib', 'base58']
-            # Todo: Set that urllib2 is not installed from pip for Python3
+required = ['requests>=1.0.0', 'impacket>=0.9.0', 'slackclient', 'progressbar', 'numpy', 'PIL', 'pytube',
+            'PyCrypto', 'base58']
+# Todo: Set that urllib2 is not installed from pip for Python3
 
 
 if __name__ == '__main__':
@@ -25,29 +24,30 @@ if __name__ == '__main__':
     long_desc = open('README.md').read() + '\n\n' + open('LICENSE.md').read()
 
     setup(name='PyExfil',
-        maintainer=__author__,
-        maintainer_email='yuval@morirt.com',
-        description="""PyExfil: Python communication library over non-standard channels.""",
-        license=__license__,
-        url='https://www.github.com/ytisf/pyexfil',
-        version="1.0 RC1",
-        download_url='https://www.github.com/ytisf/pyexfil',
-        long_description=long_desc,
-        packages=['pyexfil'],
-        install_requires=required,
-        platforms='any',
-        classifiers=(
-                'Development Status :: 3 - Beta',
-                'Intended Audience :: Developers',
-                'Intended Audience :: Science/Research',
-                'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-                'Topic :: Software Development',
-                'Topic :: Scientific/Engineering',
-                'Environment :: Console',
-                'Operating System :: Microsoft :: Windows',
-                'Operating System :: POSIX',
-                'Operating System :: Unix',
-                'Operating System :: MacOS',
-                'Programming Language :: Python',
-                'Programming Language :: Python :: 2.7',)
-        )
+          maintainer=__author__,
+          maintainer_email='hio131@gmail.com',
+          description="""PyExfil: Python communication library over non-standard channels.""",
+          license=__license__,
+          url='https://www.github.com/pnck/pyexfil',
+          version="1.0rc2",
+          download_url='https://www.github.com/pnck/pyexfil',
+          long_description=long_desc,
+          packages=['pyexfil'],
+          install_requires=required,
+          platforms='any',
+          classifiers=(
+              'Development Status :: 3 - Beta',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+              'Topic :: Software Development',
+              'Topic :: Scientific/Engineering',
+              'Environment :: Console',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Operating System :: Linux',
+              'Operating System :: Unix',
+              'Operating System :: MacOS',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 3.6',)
+          )
